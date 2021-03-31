29dfe1b589dbb1945931e8bbafa736dba850b1e7
Mustafa <69982652+Kaves47@users.noreply.github.com>
2021-03-31 20:03:32 +0300

    Update sql.sql

--- kaves_ups/sql.sql
+++ kaves_ups/sql.sql
@@ -1,30 +1,4 @@
--- --------------------------------------------------------
--- Sunucu:                       127.0.0.1
--- Sunucu sürümü:                10.4.17-MariaDB - mariadb.org binary distribution
--- Sunucu İşletim Sistemi:       Win64
--- HeidiSQL Sürüm:               11.0.0.5919
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- kaveszombie için veritabanı yapısı dökülüyor
-CREATE DATABASE IF NOT EXISTS `kaveszombie` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_turkish_ci */;
-USE `kaveszombie`;
-
--- tablo yapısı dökülüyor kaveszombie.stocks
 CREATE TABLE IF NOT EXISTS `stocks` (
   `ups` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- kaveszombie.stocks: ~0 rows (yaklaşık) tablosu için veriler indiriliyor
-/*!40000 ALTER TABLE `stocks` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stocks` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
